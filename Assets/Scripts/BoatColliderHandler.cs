ff2a95159de63992ba9c4376bdd4194da68ae259
Tiago Fernandes <t.f.5@hotmail.com>
2024-03-01 22:53:41 +0100

    Update BoatColliderHandler.cs

--- Assets/Scripts/BoatColliderHandler.cs
+++ Assets/Scripts/BoatColliderHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BoatColliderHandler : MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class BoatColliderHandler : MonoBehaviour
         if(other.gameObject.CompareTag("Ball"))
         {
             Debug.Log("Ball hit");
+            SceneManager.LoadScene("MainMenu");
         }
         if (other.gameObject.CompareTag("Cheese"))
         {
